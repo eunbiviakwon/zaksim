7e2e0d2f27d3377ee2f803391055f4942bb78014
eunbiviakwon <eunbiviakwon@gmail.com>
2020-03-09 17:50:07 +0900

    posts Model 작성

---
+++ app/posts/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+from members.models import User
+
+
+class Post(models.Model):
+    """
+    인스타그램의 포스트
+    """
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+    like_users = models.ManyToManyField(
+        User, through='PostLike', related_name='like_post_set',
+    )
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    image = models.ImageField()
+
+
+class PostComment(models.Model):
+    post = models.ForeignKey(Post, ondelete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+
+
+class PostLike(models.Model):
+    """
+    사용자가 좋아요 누른 Post 정보를 저장
+    Many-to-many필드를 중간모델(Intermediate Model)을 거쳐 사용
+    언제 생성되었는지를 Extra Field로 저장(created)
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
