698eec1c1b043904ad27c6c9874eb437aa6b37e6
eunbiviakwon <eunbiviakwon@gmail.com>
2020-03-10 16:54:21 +0900

    login 기능 구현

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from . import views
 
-app_name = "members"
-urlpatterns=[
+app_name = 'members'
+urlpatterns = [
     path('login/', views.login_view, name='login'),
 ]
--- app/members/views.py
+++ app/members/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print('username:', username)
+        print('password:', password)
+        user = authenticate(request, username=username, password=password)
+        print('user:', user)
+        if user:
+            login(request, user)
+            return redirect('index')
+        else:
+            return redirect('members:login')
     return render(request, 'members/login.html')
